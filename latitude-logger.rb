bf45473099b6e97d212d7e81694cd904bfe1ab99
Sho Hashimoto <hashimoto@shokai.org>
2011-07-04 14:00:55 +0900

    check timeStamp when store

--- latitude-logger.rb
+++ latitude-logger.rb
@@ -2,11 +2,12 @@
 require 'rubygems'
 require File.dirname(__FILE__)+'/helper'
 require 'open-uri'
+require 'pp'
 
 url = "http://www.google.com/latitude/apps/badge/api?user=#{@@conf['user_id']}&type=json"
 
 begin
-  p data = JSON.parse(open(url).read.toutf8)
+  pp data = JSON.parse(open(url).read.toutf8)
 rescue Timeout::Error  => e
   STDERR.puts e
   exit 1
@@ -15,7 +16,14 @@ rescue => e
   exit 1
 end
 
+time = data['features'][0]['properties']['timeStamp']
+
+if @@db['locations'].find({:features => {:$ne =>{:timeStamp => time}}}).count > 0
+  puts 'timeStamp #{time} already stored..'
+  exit
+end
+
 bson_id = @@db['locations'].insert(data)
 if bson_id
-  puts "save! => #{bson_id}"
+  puts "timeStamp #{time} save! => #{bson_id}"
 end
