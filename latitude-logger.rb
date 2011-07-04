c6466c4e5148d9e88aa55444b1e418d5dfdea5ed
Sho Hashimoto <hashimoto@shokai.org>
2011-07-04 14:26:53 +0900

    fix logger schema

--- latitude-logger.rb
+++ latitude-logger.rb
@@ -3,6 +3,7 @@ require 'rubygems'
 require File.dirname(__FILE__)+'/helper'
 require 'open-uri'
 require 'pp'
+require 'activesupport'
 
 url = "http://www.google.com/latitude/apps/badge/api?user=#{@@conf['user_id']}&type=json"
 
@@ -16,14 +17,26 @@ rescue => e
   exit 1
 end
 
-time = data['features'][0]['properties']['timeStamp']
 
-if @@db['locations'].find({:features => {:$ne =>{:timeStamp => time}}}).count > 0
-  puts 'timeStamp #{time} already stored..'
+feature = data['features'][0]
+props = feature['properties']
+
+location = {
+  :geo_lat => feature['geometry']['coordinates'][1],
+  :geo_lon => feature['geometry']['coordinates'][0],
+  :user_id => props['id']
+}
+props.each{|k,v|
+  location[k.underscore.to_sym] = v
+}
+
+time = location[:time_stamp]
+if @@db['locations'].find({:time_stamp => time}).count > 0
+  puts "timeStamp #{time} already stored.."
   exit
 end
 
-bson_id = @@db['locations'].insert(data)
+bson_id = @@db['locations'].insert(location)
 if bson_id
   puts "timeStamp #{time} save! => #{bson_id}"
 end
