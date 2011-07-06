109ef4fbd86ecf996c1e1491e918e24800e5f0f0
Sho Hashimoto <hashimoto@shokai.org>
2011-07-07 05:45:57 +0900

    speed

--- tools/speed.rb
+++ tools/speed.rb
@@ -5,22 +5,56 @@ require File.dirname(__FILE__)+'/../helper'
 
 R = 6378137
 
-def distance(lat_a, lon_a, lat_b, lon_b)
-  d_lon = (lon_a - lon_b).abs*(Math::PI / 180) # 経度
-  d_lat = (lat_a - lat_b).abs*(Math::PI / 180) # 緯度
-  d_y = R * d_lon * Math.cos(lat_a*(Math::PI/180))
+def distance(a, b)
+  d_lon = (a.geo_lon - b.geo_lon).abs*(Math::PI/180) # 経度
+  d_lat = (a.geo_lat - b.geo_lat).abs*(Math::PI/180) # 緯度
+  d_y = R * d_lon * Math.cos(a.geo_lat*(Math::PI/180))
   d_x = R * d_lat
   Math.sqrt(d_x*d_x + d_y*d_y)
 end
 
 def speed(a, b)
-  a_b = distance(a.geo_lat, a.geo_lon, b.geo_lat, b.geo_lon)
+  a_b = distance(a, b)
   a_b/(a.time_stamp-b.time_stamp)*60*60/1000 # km/h
 end
 
-locs = Location.desc(:time_stamp)
+def direction(a, b)
+  dx = (a.geo_lon-b.geo_lon).abs
+  dy = (a.geo_lat-b.geo_lat).abs
+  if dx*0.7 > dy
+    if a.geo_lon > b.geo_lon
+      return '東'
+    else
+      return '西'
+    end
+  elsif dx*1.3 < dy
+    if a.geo_lat > b.geo_lat
+      return '北'
+    else
+      return '南'
+    end
+  else
+    if a.geo_lon > b.geo_lon
+      if a.geo_lat > b.geo_lat
+        return '北東'
+      else
+        return '北西'
+      end
+    else
+      if a.geo_lat > b.geo_lat
+        return '南東'
+      else
+        return '南西'
+      end
+    end
+  end
+end
+
+locs = Location.desc(:time_stamp).limit(40)
 for i in 0...locs.size-1 do
   s = speed(locs[i], locs[i+1])
   time = Time.at locs[i].time_stamp
-  puts "#{s}(km/h), #{locs[i+1].reverse_geocode}, #{time}"
+  next if locs[i].time_stamp - locs[i+1].time_stamp > 60*3
+  dir = direction(locs[i], locs[i+1])
+  puts "#{s}(km/h), #{dir}, #{locs[i+1].reverse_geocode}, #{time}"
 end
