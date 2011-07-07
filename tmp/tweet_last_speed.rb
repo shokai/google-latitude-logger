5f01cfcca2997e73069c2811d51590b8c045d2e8
Sho Hashimoto <hashimoto@shokai.org>
2011-07-07 23:08:39 +0900

    tweet speed

---
+++ tmp/tweet_last_speed.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require File.dirname(__FILE__)+'/../helper'
+
+R = 6378137
+
+def distance(a, b)
+  d_lon = (a.geo_lon - b.geo_lon).abs*(Math::PI/180) # 経度
+  d_lat = (a.geo_lat - b.geo_lat).abs*(Math::PI/180) # 緯度
+  d_y = R * d_lon * Math.cos(a.geo_lat*(Math::PI/180))
+  d_x = R * d_lat
+  Math.sqrt(d_x*d_x + d_y*d_y)
+end
+
+def speed(a, b)
+  a_b = distance(a, b)
+  a_b/(a.time_stamp-b.time_stamp)*60*60/1000 # km/h
+end
+
+def direction(a, b)
+  dx = (a.geo_lon-b.geo_lon).abs
+  dy = (a.geo_lat-b.geo_lat).abs
+  if dx*0.7 > dy
+    if a.geo_lon > b.geo_lon
+      return '東'
+    else
+      return '西'
+    end
+  elsif dx*1.3 < dy
+    if a.geo_lat > b.geo_lat
+      return '北'
+    else
+      return '南'
+    end
+  else
+    if a.geo_lon > b.geo_lon
+      if a.geo_lat > b.geo_lat
+        return '北東'
+      else
+        return '北西'
+      end
+    else
+      if a.geo_lat > b.geo_lat
+        return '南東'
+      else
+        return '南西'
+      end
+    end
+  end
+end
+
+a, b = Location.desc(:time_stamp).limit(2)
+sp = speed(a, b)
+dir = direction(a, b)
+puts Time.at(a.time_stamp)
+puts cmd = "/usr/local/sbin/tweet_log '時速#{sp}Kmで#{dir}に移動中'"
+if Time.now.to_i - a.time_stamp > 60*20 or sp < 1
+  puts 'not tweet'
+  exit
+end
+system cmd
